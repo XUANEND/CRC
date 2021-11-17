88d503282dbaaece129103707b511024551aa4bb
XUANEND <1540159121@qq.com>
2021-11-17 16:34:46 +0800

    packages install & readme update

---
+++ code/0_install_packages.R
@@ -0,0 +1,12 @@
+# install packages 
+packages.list <- c(
+  "ConsensusClusterPlus",
+  "survival",
+  "survminer",
+  "limma",
+  "clusterFilter",
+  "ggplot2",
+  "maftools",
+  "pRRophetic",
+  "rms"
+)
